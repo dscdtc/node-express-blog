767b8268ebde03cc7d5b114d73cca2c52806f690
dscdtc <dscdtc@users.noreply.github.com>
2017-05-04 16:23:55 +0800

    Delete production.js

--- config/production.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    port: 3000,
-    session: {
-        secret: 'myblog',
-        key: 'dscdtc$201705',
-        maxAge:2592000000
-    },
-    mongodb: 'mongodb://myblog:myblog-1753@ds137090.mlab.com:37090/dscdtc'
-};
